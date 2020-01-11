a13bf518b8af3ae17c51b1980af7f8c2f9b10939
Matthew C <m23.cullen@gmail.com>
2020-01-11 03:13:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ function mapToNegativize(srcRay){
   return result;
 }
 
+function mapToNoChange(srcRay){
+  let result = [];
+  
+  for(let i = 0; i < srcRay.length; i++){
+    result.push(srcRay[i]);
+  }
+}
+
+function mapToDouble(srcRay){
+  let result = [];
+  
+  for(let i = 0; i < srcRay.length; i++){
+    result.push(srcRay[i] * 2);
+  }
+}
+
+function mapToSquare(srcRay){
+  let result = [];
+  
+  for(let i = 0; i < srcRay.length; i++){
+    result.push(srcRay[i] ** 2);
+  }
+}
